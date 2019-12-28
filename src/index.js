f3a80f836c49cac27e09dd11035327199dd1e5cc
Marcos Gabriel Robles Gonzalez <mrobles@solomediagroup.co>
2019-12-28 13:22:06 -0500

    removed topologys

--- src/index.js
+++ src/index.js
@@ -3,10 +3,9 @@ const morgan = require('morgan');
 const mongoose = require('mongoose');
 const app = express();
 
-mongoose.connect('mongodb://properuser:zrMiVOjGKPm3mGU6@propertaskdb-shard-00-00-zkejv.mongodb.net:27017,propertaskdb-shard-00-01-zkejv.mongodb.net:27017,propertaskdb-shard-00-02-zkejv.mongodb.net:27017/test?ssl=true&replicaSet=propertaskdb-shard-0&authSource=admin&retryWrites=true&w=majority'
-    /*'mongodb://localhost/propertask-db'*/, {
-    useNewUrlParser: true, 
-    useUnifiedTopology: true    
+mongoose.connect('mongodb://properuser:zrMiVOjGKPm3mGU6@propertaskdb-shard-00-00-zkejv.mongodb.net:27017,propertaskdb-shard-00-01-zkejv.mongodb.net:27017,propertaskdb-shard-00-02-zkejv.mongodb.net:27017/test?ssl=true&replicaSet=propertaskdb-shard-0&authSource=admin&retryWrites=true&w=majority', {
+    useNewUrlParser: true
+    //useUnifiedTopology: true    
 })
 .then(db => console.log('db is connected'))
 .catch(err => console.error(err))
